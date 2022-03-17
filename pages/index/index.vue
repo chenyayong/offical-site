31dd53a23bff4feef90f04cd7a27f2f144df5e5b
陈亚勇 <chenyayong@chenyayongdeMBP.lan>
2022-03-18 00:06:28 +0800

    pc网站的基本版本

---
+++ pages/index/index.vue
@@ -0,0 +1,226 @@
+<template>
+	<view class="">
+		<section class="pageOne">
+			<img src="@/static/img/bg01.jpeg" alt="" class="bg" />
+			<div class="content">
+				<div class="imgBox"><!-- <img src="/static/img/phone01.png" alt="" /> --></div>
+				<div class="one-right">
+					<div class="one-right-top">
+						<!-- <img src="/static/img/icon.png" class="icon" alt="当乐icon" /> -->
+						<img src="@/static/logo.png" class="icon" alt="新视界icon" style="border-radius: 50%;width: 200px;height: 200px" />
+						<!-- <img src="/static/img/logo.png" class="logo" alt="当乐logo" /> -->
+					</div>
+					<p class="text-desc">以全新的方式去做自己喜欢的事情，NV新视界，专注于精品商品分享，我们致力于为您发掘更多精品，让你尽享购物美好世界。</p>
+					<!-- <div class="one-right-ctr">
+						<p class="fl">
+							<span>版本：8.8.3</span>
+							<i>丨</i>
+							<span>大小：23.2M</span>
+							<span>发布时间：2021-10-21</span>
+							<i>丨</i>
+							<a href="https://app.d.cn/android/update" class="update">更新记录</a>
+						</p>
+					</div> -->
+					<!-- <div class="one-right-btm">
+						<div class="imgbox"><img src="img/qrcode.png" alt="" /></div>
+						<div class="links">
+							<a href="https://res9.d.cn/android/yxzx.apk" class="download">立即下载</a>
+							<a href="" class="test">参与Android 移动端 Beta版测试</a>
+						</div>
+						<a href="javascript:" class="play" onclick="windowopen('http://video.d.cn/shiwan/CreativeVideo/APPNewEdition.mp4')">
+							<img src="/static/img/play.png" alt="" />
+						</a>
+					</div> -->
+				</div>
+			</div>
+		</section>
+		<section class="pageTwo page active">
+			<div class="content">
+				<h1 class="text-title two-title">
+					NV新视界
+					<br />
+					你喜欢的和将会爱上的各种
+					<br />
+					商品，都能在这里找到
+				</h1>
+				<div class="imgwrap">
+					<div  style="display: flex;justify-content: center;">
+						<img src="@/static/img/fenlei2.jpeg" style="width: 350px;" alt="" />
+						<!-- <img src="/static/img/sec-p2.png" alt="" />
+						<img src="/static/img/sec-p3.png" alt="" />
+						<img src="/static/img/sec-p4.png" alt="" />
+						<img src="/static/img/sec-p5.png" alt="" /> -->
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- <section class="pageThree page active">
+			<div class="content">
+				<h3 class="text-title">
+					一键
+					<span class="text-yellow">谷歌服务</span>
+					，助你畅玩游戏。
+				</h3>
+				<p class="text-desc">
+					通过当乐APP一键安装谷歌三件套，为你打开更广阔的的游戏大门。该功能目前仍处测试阶段， 我们将持续更新，希望得到你的使用反馈。
+				</p>
+				<div class="imgbox">
+					<img class="img-ipad" src="/static/img/three-ipad.png" alt="" />
+					<img class="img-phone" src="/static/img/three-phone.png" alt="" />
+				</div>
+			</div>
+		</section> -->
+		<section class="pageFour page active">
+			<div class="content">
+				<h3 class="text-title">
+					<span class="text-yellow">排行分类</span>
+					优化，更加懂你所爱。
+				</h3>
+				<p class="text-desc">通过专业的数据统计，我们实时呈现商品热度排名；更直观的商品分类筛选， 让你尽可能快速找到优质商品。</p>
+				<div style="display: flex;justify-content: center;">
+					<img src="@/static/img/fenlei.jpeg" style="width: 350px;" alt="" />
+					<!-- <img src="/static/img/f-phone01.jpeg" alt="" />
+					<img src="/static/img/f-phone02.jpeg" alt="" />
+					<img src="/static/img/f-phone03.png" alt="" />
+					<img src="/static/img/f-phone04.jpeg" alt="" />
+					<img src="/static/img/f-phone05.jpeg" alt="" /> -->
+				</div>
+				<!-- <div class="ranks">
+					<div class="textbox">
+						<p>新游排行</p>
+						<span>当下玩家中人气最高的手游，了解你身边的人都在玩什么。</span>
+					</div>
+					<div class="textbox">
+						<p>热游排行</p>
+						<span>根据下载量，游戏新鲜度等综合计算得出。</span>
+					</div>
+					<div class="textbox">
+						<p>预约排行</p>
+						<span>展示当前玩家最受期待新游，掌握最权威的新游信息。</span>
+					</div>
+				</div> -->
+			</div>
+		</section>
+		<!-- <section class="pageFive page active">
+			<div class="content content-row">
+				<div class="c-left">
+					<h1 class="text-title">
+						全新
+						<span class="text-yellow">话题讨论</span>
+						,
+						<br />
+						表达你的观点。
+					</h1>
+					<p class="text-desc">
+						甄选热门商品话题，只为创造独特视角。通过参与话题 讨论，不仅可以获取金币奖励，还能探寻时下热点信息。
+					</p>
+				</div>
+				<div class="c-right">
+					<div class="imgbox">
+						<img class="phone" src="/static/img/ht-phone.png" alt="" />
+						<img class="comt" src="/static/img/img-comt01.png" alt="" />
+						<img class="comt" src="/static/img/img-comt02.png" alt="" />
+						<img class="comt" src="/static/img/img-comt03.png" alt="" />
+						<img class="comt" src="/static/img/img-comt04.png" alt="" />
+						<img class="comt" src="/static/img/img-comt05.png" alt="" />
+						<img class="comt" src="/static/img/img-comt06.png" alt="" />
+					</div>
+				</div>
+			</div>
+		</section>
+		 -->
+		<!-- <section class="pageSix page active">
+			<div class="content content-row">
+				<div class="c-lft"><img src="/static/img/six-phone.png" alt="" /></div>
+				<div class="c-rht">
+					<h1 class="text-title">
+						<span class="text-yellow">专题模块</span>
+						升级，
+						<br />
+						内容丰富多彩。
+					</h1>
+					<p class="text-desc">全新专题模块，推荐更多好游，再也不用担心找不到好 游戏。现在你可以收藏喜欢的专题，或者将它们分享给 身边的好友。</p>
+				</div>
+			</div>
+		</section> -->
+		<!-- <section class="pageSeven page active">
+			<div class="content content-row">
+				<div class="c-lft">
+					<h1 class="text-title">
+						新增
+						<span class="text-yellow">游戏频道</span>
+						，
+						<br />
+						手游一网打尽。
+					</h1>
+					<p class="text-desc">
+						实时查看最新上架、最近更新游戏，获取网游开服开测 时间。在游戏频道，你可以探索更多，通过不同类型的 列表分类，为你呈现最好的游戏。
+					</p>
+					<div class="channel"><img src="/static/img/seven-channel.png" alt="" /></div>
+				</div>
+				<div class="c-rht"><img src="/static/img/seven-phone.png" alt="" /></div>
+			</div>
+		</section>
+		 -->
+		<section class="pageEight page active">
+			<div class="content content-row">
+				<div class="c-lft" style="opacity: 0;"><img src="@/static/img/eight-phone.png" alt="" /></div>
+				<div class="c-rht">
+					<h1 class="text-title">
+						<span class="text-yellow">专业评分</span>
+						上线，
+						<br />
+						品质保障
+					</h1>
+					<p class="text-desc">
+						专业的评分系统，给出最直观的评价。我们综合 每一位的评论、打分，你可直接判断品质，亦 或通过评论表达不一样的看法。
+					</p>
+					<!-- <div class="imgbox">
+						<img src="/static/img/eight-img01.png" alt="" />
+						<img src="/static/img/eight-img02.png" alt="" />
+						<img src="/static/img/eight-img03.png" alt="" />
+					</div> -->
+				</div>
+			</div>
+		</section>
+		<footer style="text-align: center">
+			<ul class="text-center">
+				<li><a href="https://beian.miit.gov.cn/#/Integrated/index" class="hover" target="_blank">粤ICP备2021103276号</a></li>
+				<!-- <li>
+					<a class="beian" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802035340" target="_blank">
+						京公网安备：11010802035340号
+					</a>
+				</li> -->
+				<li><a href="#/pages/about/index?index=0" class="hover">关于快来</a></li>
+				<!-- <li><a href="http://www.d.cn/en-us/" class="hover">About Downjoy</a></li> -->
+				<li><a href="#/pages/about/index?index=1" class="hover">联系我们</a></li>
+				<!-- <li><a href="http://open.d.cn/" class="hover">开放平台</a></li> -->
+				<li><a href="#/pages/about/index?index=2" class="hover">隐私保护</a></li>
+				<!-- <li><a href="http://www.d.cn/about/sitemap.html" class="hover">网站地图</a></li> -->
+			</ul>
+			<p class="text-center">
+				Copyright © 2017 -
+				<span class="currentYear">2021</span>
+				. All Rights Reserved. 深圳快来互娱网络科技有限公司 版权所有
+			</p>
+		</footer>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {
+		const style = document.documentElement.style;
+		style.fontSize = '142.5px';
+	},
+	methods: {}
+};
+</script>
+<style>
+@import '@/common/css/index.css';
+</style>
